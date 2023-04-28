d0c7414609df729d212dc39a07d28461097579e4
sudipto <fedev.sudipto@gmail.com>
2023-04-28 22:13:31 +0530

    mongo query eercises

---
+++ MongoDbQueryExercises.js
@@ -0,0 +1,14 @@
+/**Mongo Query Exercises, Collection: restaurants */
+
+//write a query to display the fields resturant_id, borough and cuisine, but exclude the field _id
+
+db.getCollection("restaurants").aggregate([
+    {
+        "$project":{
+            "_id":0,
+            "restaurant_id":1,
+            "borough":1,
+            "cuisine":1
+        }
+    }
+])
