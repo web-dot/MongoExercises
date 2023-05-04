cfb2748b6df988f7b39620bac4de3d38e76cf638
sudipto <cg.sudipc@gmail.com>
2023-05-04 23:34:10 +0530

    aggregation query test

--- AggregationTest-ComGuiMapper.js
+++ AggregationTest-ComGuiMapper.js
@@ -41,5 +41,131 @@ db.getCollection('dhi_committee_guideline_mapper').aggregate([
             "foreignField":"_id",
             "as":"committees"
         }
-    }
+    },
+    // {
+    //     "$unwind": "$committees"
+    //   },
+    //   {
+    //     "$project": {
+    //       "eventLocalField": {
+    //         "$map": {
+    //           "input": {
+    //             "$map": {
+    //               "input": "$committees.events",
+    //               "in": {
+    //                 "$arrayElemAt": [
+    //                   {
+    //                     "$objectToArray": "$$this"
+    //                   },
+    //                   1
+    //                 ]
+    //               }
+    //             }
+    //           },
+    //           "in": "$$this.v"
+    //         }
+    //       }
+    //     }
+    //   },
+    //   {
+    //     "$unwind": {
+    //       "path": "$eventLocalField",
+    //       "preserveNullAndEmptyArrays": true
+    //     }
+    //   },
+    //   {
+    //     "$lookup": {
+    //       "from": "dhi_event",
+    //       "localField": "eventLocalField",
+    //       "foreignField": "_id",
+    //       "as": "events"
+    //     }
+    //   },
+    //   {
+    //     "$unwind": "$events"
+    //   },
+    //   {
+    //     "$match": {
+    //       "events.academicYear": "2021-22",
+    //       "events.eventStatus": "Completed"
+    //     }
+    //   },
+  //     {
+  //       "$unwind": "$events.participants"
+  //     },
+  //     {
+  //       "$lookup": {
+  //         "from": "dhi_user",
+  //         "localField": "events.participants._id",
+  //         "foreignField": "_id",
+  //         "as": "user"
+  //       }
+  //     },
+  //     {
+  //       "$unwind": "$user"
+  //     },
+  //     {
+  //       "$match": {
+  //         "user.userType": "STUDENT"
+  //       }
+  //     },
+  //     {
+  //       "$group": {
+  //         "_id": "$events._id",
+  //         "eventDesc": {
+  //           "$first": {
+  //             "$ifNull": [
+  //               "$events.description",
+  //               "-"
+  //             ]
+  //           }
+  //         },
+  //         "eventName": {
+  //           "$first": "$events.name"
+  //         },
+  //         "eventId": {
+  //           "$first": "$events._id"
+  //         },
+  //         "eventFromDate": {
+  //           "$first": "$events.fromDate"
+  //         },
+  //         "eventToDate": {
+  //           "$first": "$events.toDate"
+  //         },
+  //         "user": {
+  //           "$push": "$$ROOT.user"
+  //         },
+  //         "eventType": {
+  //           "$first": "$events.eventType"
+  //         },
+  //         "eventLevel": {
+  //           "$first": "$events.level"
+  //         }
+  //       }
+  //     },
+  //     {
+  //       "$project": {
+  //         "eventDesc": {
+  //           "$ifNull": [
+  //             "$eventDesc",
+  //             "-"
+  //           ]
+  //         },
+  //         "eventName": "$eventName",
+  //         "eventId": "$eventId",
+  //         "eventFromDate": "$eventFromDate",
+  //         "eventToDate": "$eventToDate",
+  //         "eventType": "$eventType",
+  //         "eventLevel": "$eventLevel",
+  //         "studentCount": {
+  //           "$size": "$user"
+  //         }
+  //       }
+  //     },
+  //     {
+  //       "$sort": {
+  //         "eventLevel": 1
+  //       }
+  //     }
+  // 
 ])
